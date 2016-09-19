ce71c00f8f1d76705b70b3f000b0ae6e452b0003
Shockfire <shock@420blaze.it>
2016-09-19 19:38:27 -0400

    Basic StringId unit tests

---
+++ FiveLib.Tests/Ausar/Tags/StringIdTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiveLib.Ausar.Tags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FiveLib.Tests.Ausar.Tags
+{
+    [TestClass]
+    public class StringIdTests
+    {
+        [TestMethod]
+        public void TestConstructStringIdFromUInt()
+        {
+            var stringId = new StringId(1337);
+            Assert.AreEqual(1337U, stringId.Value);
+        }
+
+        [TestMethod]
+        public void TestConstructStringIdFromString()
+        {
+            // We can assume that the murmurhash library works, so this doesn't need to be very extensive
+            var stringId = new StringId("__default__");
+            Assert.AreEqual(stringId.Value, 0x9B555AD2);
+        }
+    }
+}
